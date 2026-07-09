1ede5658b2ab78fa2dca9f8705ccb1879aae0ba7
Abdel samad <127221616+Ze-one@users.noreply.github.com>
2026-07-09 08:49:07 +0100

    Mount admin pass regeneration control

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import ProfileControl from './components/ProfileControl.tsx';
+import PassRegeneratorControl from './components/PassRegeneratorControl.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <ProfileControl />
+    <PassRegeneratorControl />
   </StrictMode>,
 );
