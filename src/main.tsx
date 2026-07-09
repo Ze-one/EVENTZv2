a511eb9d68545ab424ffebde8c6a17f810046c08
Abdel samad <127221616+Ze-one@users.noreply.github.com>
2026-07-09 09:38:09 +0100

    Replace floating profile control with header account control

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import ProfileControl from './components/ProfileControl.tsx';
 import PassRegeneratorControl from './components/PassRegeneratorControl.tsx';
 import NotificationCenter from './components/NotificationCenter.tsx';
+import HeaderAccountControl from './components/HeaderAccountControl.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <ProfileControl />
     <PassRegeneratorControl />
     <NotificationCenter />
+    <HeaderAccountControl />
   </StrictMode>,
 );
