8fca91eaf95b91b1facf3e547b08db350afa8f82
Abdel samad <127221616+Ze-one@users.noreply.github.com>
2026-07-09 09:43:42 +0100

    Remove floating regenerate pass control from app mount

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import PassRegeneratorControl from './components/PassRegeneratorControl.tsx';
 import NotificationCenter from './components/NotificationCenter.tsx';
 import HeaderAccountControl from './components/HeaderAccountControl.tsx';
 import './index.css';
@@ -9,7 +8,6 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <PassRegeneratorControl />
     <NotificationCenter />
     <HeaderAccountControl />
   </StrictMode>,
